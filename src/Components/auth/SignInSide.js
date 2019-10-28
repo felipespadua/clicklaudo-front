d36886c2207ae5ddafbf0f27967f4137a27dff00
Felipe Padua <felipepadua@Mariannas-MBP.localdomain>
2019-10-28 19:55:23 -0300

    ajuste no signin

--- src/Components/auth/SignInSide.js
+++ src/Components/auth/SignInSide.js
@@ -125,7 +125,7 @@ export default function SignInSide(props) {
             </Button>
             <Grid container>
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link href="/cadastro" variant="body2">
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
