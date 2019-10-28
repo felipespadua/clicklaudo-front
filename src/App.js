b2d753a7c6ce77f871e1c2ed123c859a13a0c694
marcos azevedo <marcosaalacerda@gmail.com>
2019-10-28 18:30:29 -0300

    top

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
-import logo from './logo.svg';
 import VoiceRecognition from './Components/VoiceRecognition';
 import './App.css';
 import Login from './Components/auth/Login'
