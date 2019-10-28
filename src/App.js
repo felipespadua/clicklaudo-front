28fdbe8759ed8fb9848408f09d37913f05ac5344
Rafael Dias <rafaels.dias@me.com>
2019-10-28 14:57:40 -0300

    correcao de bugs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
-import logo from './logo.svg';
+// import logo from './Logo.svg';
 import VoiceRecognition from './Components/VoiceRecognition';
 import './App.css';
 import Login from './Login'
 import Cadastro from './Cadastro'
 import Laudos from './Laudos'
-import MainTable from './MainTable'
+// import MainTable from './MainTable'
 import { Fragment } from 'react'
 import SimpleExpansionPanel from "./Expansion";
 
@@ -25,7 +25,7 @@ class App extends React.Component {
               <Route exact path="/" component={Login} />
               <Route path="/cadastro" component={Cadastro} />
               <Route path="/laudos" component={Laudos} />
-              <Route path="/maintable" component={MainTable} />
+              {/* <Route path="/maintable" component={MainTable} /> */}
               <Route path="/simpleExpansion" component={SimpleExpansionPanel} />
 
           </Switch>
