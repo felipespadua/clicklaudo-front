58a3d5546ef30008e554d8107288fc855fabfc59
Rafael Dias <rafaels.dias@me.com>
2019-10-29 15:14:58 -0300

    pagina signup arrumada

--- src/Components/auth/SignInSide.js
+++ src/Components/auth/SignInSide.js
@@ -3,11 +3,8 @@ import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import Paper from "@material-ui/core/Paper";
-import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
@@ -134,7 +131,7 @@ export default function SignInSide(props) {
             </Button>
             <Grid container>
               <Grid item>
-                <Link href="/SignUp" variant="body2">
+                <Link href="/signup" variant="body2">
                   {"Não possui uma conta? Sign Up"}
                 </Link>
               </Grid>
