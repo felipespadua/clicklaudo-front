bb6c8f3c3d0cbd16005a0cd69b5422aca6ee10f1
Felipe Padua <felipepadua@Mariannas-MBP.localdomain>
2019-10-29 14:25:44 -0300

    ajuste no nome navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -45,7 +45,7 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-export default function NavBar() {
+export default function Navbar() {
   
   const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState(null);
